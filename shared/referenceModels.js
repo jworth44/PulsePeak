2e9f41615f8a12694ac418021b2d8e57bf7eedfd
James Worthing <jworthing44@gmail.com>
2026-04-24 22:55:19 -0500

    Lock PulsePeak reference model identities

---
+++ shared/referenceModels.js
@@ -0,0 +1,8 @@
+// DO NOT MODIFY WITHOUT EXPLICIT APPROVAL
+// Locked PulsePeak reference identities for all exercise visuals
+// These files are the visual identity anchors for male/female exercise generation
+
+export const REFERENCE_MODELS = {
+  female: "/media/models/reference_female.png",
+  male: "/media/models/reference_male.png",
+};
